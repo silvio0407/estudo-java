357c4df12887d602141785bd4f5a9d5050ebd95f
Silvio <silvio0407@gmail.com>
2019-08-27 10:52:14 +0100

    file updated

--- BinarySearchDemo/src/br/com/example/howto/SetList.java
+++ BinarySearchDemo/src/br/com/example/howto/SetList.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class SetList {
 
@@ -22,8 +23,21 @@ public class SetList {
 		str2.add("b");
 		str2.add("c");
 		
+		List<Integer> str3 = new ArrayList<>();
+		
+		str3.add(1);
+		str3.add(2);
+		
+		List<Integer> str4 = new ArrayList<>();
+		
+		str4.add(1);
+		str4.add(2);
+		str4.add(3);
+		
 		setLits.add(str);
 		setLits.add(str2);
+		setLits.add(str3.stream().map(String::valueOf).collect(Collectors.toList()));
+		setLits.add(str4.stream().map(String::valueOf).collect(Collectors.toList()));
 		
 		
 		setLits.forEach(System.out::println);
